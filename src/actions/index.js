94feb5b5d8b427a276125e44d6ac68bb58c5c505
Gavin Dreyer <gavindreyer1@gmail.com>
2020-05-13 11:49:43 -0700

    changed verbs request to env var

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export const FETCH_FAILURE = 'FETCH_FAILURE';
 export const fetchVerbs = history => dispatch => {
 	dispatch({ type: START_FETCHING });
 	AxiosWithAuth()
-		.get(`http://localhost:4444/api/tense/`)
+		.get(`${process.env.REACT_APP_HEROKU_API}/api/tense/`)
 		.then(res => dispatch({ type: FETCH_SUCCESS, payload: res.data }))
 		.catch(err => {
 			dispatch({ type: FETCH_FAILURE, payload: err.response });
